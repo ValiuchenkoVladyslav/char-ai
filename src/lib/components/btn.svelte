06da00cb0bc928a5e57634b95db9626c210fd656
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-05-20 13:44:04 -0700

    add btn and input components

---
+++ src/lib/components/btn.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from "svelte/elements";
+
+  interface BtnProps extends HTMLButtonAttributes {
+    dark?: boolean;
+    class?: string;
+  }
+
+  const { dark, children, class: _class, ...props }: BtnProps = $props();
+</script>
+
+<button class={["btn", dark ? "dark" : "light", _class]} {...props}>
+  {@render children?.()}
+</button>
---
+++ src/lib/components/input.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { HTMLInputAttributes } from "svelte/elements";
+
+  interface InputProps extends Omit<HTMLInputAttributes, "id"> {
+    dark?: boolean;
+    class?: string;
+    error?: string;
+    label?: string;
+    name: string;
+  }
+
+  const { label, error, class: _class, dark, ...props }: InputProps = $props();
+</script>
+
+<section class="flex flex-col gap-2">
+  <div class="flex justify-between">
+    {#if label}
+      <label for={props.name}>
+        {label}
+      </label>
+    {/if}
+
+    {#if error}
+      <p class="text-red-600 text-sm" aria-roledescription="error">
+        * {error}
+      </p>
+    {/if}
+  </div>
+
+  <input
+    class={["input", dark ? "dark" : "light", _class]}
+    id={props.name}
+    {...props}
+  />
+</section>
