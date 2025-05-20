e84e9a5b6a408ee82289b05f0992cc7c4ca9d0ab
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-05-20 15:43:30 -0700

    add email module

---
+++ src/lib/server/email.ts
@@ -0,0 +1,44 @@
+import { waitUntil } from "@vercel/functions";
+import { type CreateEmailOptions, Resend } from "resend";
+
+import type {
+	Component,
+	ComponentProps,
+	ComponentType,
+	SvelteComponent,
+} from "svelte";
+import { render } from "svelte/server";
+
+import { env } from "$env/dynamic/private";
+
+const resend = new Resend(env.RESEND_API satisfies string);
+
+/** send email without execution blocking */
+export function sendEmail<
+	// biome-ignore lint/suspicious/noExplicitAny: exact type needed
+	Comp extends Component<any>,
+>(
+	to: CreateEmailOptions["to"],
+	subject: CreateEmailOptions["subject"],
+	// biome-ignore lint/suspicious/noExplicitAny: exact type needed
+	template: Comp extends SvelteComponent<any> ? ComponentType<Comp> : Comp,
+	templateProps: ComponentProps<Comp>,
+) {
+	async function renderAndSendTask() {
+		const output = render(template, { props: templateProps });
+		console.log("OUTPUT", output.body);
+		console.log("OUTPUT::HEAD", output.head);
+		const res = await resend.emails.send({
+			from: "Char Ai <onboarding@resend.dev>",
+			to,
+			subject,
+			html: output.body,
+		});
+
+		if (res.error) {
+			console.error("FAILED TO SEND EMAIL:", res.error);
+		}
+	}
+
+	waitUntil(renderAndSendTask());
+}
