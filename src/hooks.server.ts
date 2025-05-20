9f201cf18aa2fcec4c255b45c5b1fa237f49f131
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-05-20 10:42:16 -0700

    fix chrome dev tools 404

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,6 +6,15 @@ import { getToken, verifyToken } from "$lib/server/jwt";
 import { type Handle, redirect } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
+	// https://chromium.googlesource.com/devtools/devtools-frontend/+/main/docs/ecosystem/automatic_workspace_folders.md
+	// biome-ignore lint/correctness/noUnusedLabels: see vite.config.ts
+	// biome-ignore lint/suspicious/noConfusingLabels: see vite.config.ts
+	DEV: if (
+		event.url.pathname === "/.well-known/appspecific/com.chrome.devtools.json"
+	) {
+		return new Response(null, { status: 204 });
+	}
+
 	const token = getToken(event.cookies); // cheap
 
 	if (event.route.id?.startsWith("/(protected)")) {
