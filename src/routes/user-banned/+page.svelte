4b380f170abd42f15a3fb8db8707814da0436071
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-05-19 19:56:19 -0700

    add page content

--- src/routes/user-banned/+page.svelte
+++ src/routes/user-banned/+page.svelte
@@ -2,4 +2,13 @@
   import HeadMeta from "$lib/components/head-meta.svelte";
 </script>
 
-<HeadMeta noindex title="Your account has been banned"/>
+<HeadMeta noindex title="Your account has been banned" />
+
+<div class="flex h-screen items-center justify-center">
+  <div class="text-center">
+    <h1 class="text-2xl font-bold">User Banned X(</h1>
+    <p class="mt-4 text-lg">
+      Your account has been banned. Please contact our support.
+    </p>
+  </div>
+</div>
