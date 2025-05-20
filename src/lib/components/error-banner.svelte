24ab5dfeba0deb32daec6e8a9bb811e00da2c2c9
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-05-20 15:47:33 -0700

    add error banner

---
+++ src/lib/components/error-banner.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import TriangleAlert from "lucide-svelte/icons/triangle-alert";
+
+  interface Props {
+    error?: string;
+  }
+
+  const { error }: Props = $props();
+</script>
+
+{#if error}
+  <p
+    class="border-2 border-red-600/70 bg-red-600/20 rounded-md px-3 py-2 flex gap-2"
+    aria-roledescription="error"
+  >
+    <TriangleAlert />
+
+    {error}
+  </p>
+{/if}
