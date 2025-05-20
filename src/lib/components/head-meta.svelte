5fa2ae8261222e99b54ca62dd790a040808c26f2
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-05-19 18:55:43 -0700

    add metadata helper component

---
+++ src/lib/components/head-meta.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  interface Props {
+    title: string;
+    description?: string;
+    noindex?: boolean;
+  }
+
+  let props: Props = $props();
+</script>
+
+<svelte:head>
+  <title>{"CharAi | " + props.title}</title>
+
+  {#if props.description}
+    <meta name="description" content={props.description}/>
+  {/if}
+
+  {#if props.noindex}
+    <meta name="robots" content="noindex"/>
+  {/if}
+</svelte:head>
