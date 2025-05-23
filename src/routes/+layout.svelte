2ac5853acc751cff1832ae1be1bb78ecac81e188
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-05-23 01:20:17 -0700

    remove analytics & speed-insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
 	import "../app.css";
 
-  import { dev } from "$app/environment";
-
-  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
-  import { injectAnalytics } from "@vercel/analytics/sveltekit";
-
-  if (!dev) {
-    injectSpeedInsights();
-    injectAnalytics({ mode: "production" });
-  }
-
 	const { children } = $props();
 </script>
 
