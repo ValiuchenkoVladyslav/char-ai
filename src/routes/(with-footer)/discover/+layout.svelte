63990f940e2be23dc6c5b088dbee8463e5dc2dd5
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-06-03 11:25:10 -0700

    hide pagination if not needed

--- src/routes/(with-footer)/discover/+layout.svelte
+++ src/routes/(with-footer)/discover/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import { invalidate } from "$app/navigation";
+  import { page } from "$app/state";
+  import { CHARACTERS_COUNT_LOAD_KEY, CHARACTERS_PER_PAGE } from "./shared";
   import HeadMeta from "$lib/components/head-meta.svelte";
   import Search from "lucide-svelte/icons/search";
-  import { CHARACTERS_COUNT_LOAD_KEY, CHARACTERS_PER_PAGE } from "./shared";
   import ChevronRight from "lucide-svelte/icons/chevron-right";
   import ChevronsRight from "lucide-svelte/icons/chevrons-right";
-  import { page } from "$app/state";
 
   const { children, data } = $props();
 
   let query = $derived(page.url.searchParams.get("q") || "");
-  let currPage = $derived(Number(page.url.searchParams.get("p")) || 1);
+  let currPage = $derived(Number.parseInt(page.url.searchParams.get("p") || "1", 10) || 1);
   let maxPage = $state(0);
   $effect.pre(() => {
     data.charactersCount.then((count) => {
@@ -42,47 +42,47 @@
   </form>
 </div>
 
-<section class="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 px-8 lg:px-16 pt-12">
-  {@render children()}
-</section>
+{@render children()}
 
-<nav class="flex py-2 gap-2 justify-center font-bold">
-  <a
-    href="/discover?p=1{query ? `&q=${query}` : ''}"
-    class={["pagination-btn", currPage === 1 && "pointer-events-none opacity-50"]}
-  >
-    <ChevronsRight class="rotate-180" />
-  </a>
-  <a
-    href="/discover?p={currPage - 1}{query ? `&q=${query}` : ''}"
-    class={["pagination-btn", currPage === 1 && "pointer-events-none opacity-50"]}
-  >
-    <ChevronRight class="rotate-180" />
-  </a>
-
-  <!-- this offset makes current page display first by default (while current displays it as second by default) -->
-  <!-- let paginationOffset = $derived(Math.max(0, Math.min(currPage - 1, maxPage - PAGINATION_ITEMS))); -->
-  <!-- {@const page = paginationOffset + i + 1} -->
-  {#each Array(Math.min(maxPage, PAGINATION_ITEMS)) as _, i}
-    {@const page = paginationOffset + i}
+{#if maxPage > 0}
+  <nav class="flex py-2 gap-2 justify-center font-bold">
     <a
-      href="/discover?p={page}{query ? `&q=${query}` : ''}"
-      class={["pagination-btn", page === currPage && "bg-fg/20"]}
+      href="/discover?p=1{query ? `&q=${query}` : ''}"
+      class={["pagination-btn", currPage === 1 && "pointer-events-none opacity-50"]}
     >
-      {page}
+      <ChevronsRight class="rotate-180" />
     </a>
-  {/each}
+    <a
+      href="/discover?p={currPage - 1}{query ? `&q=${query}` : ''}"
+      class={["pagination-btn", currPage === 1 && "pointer-events-none opacity-50"]}
+    >
+      <ChevronRight class="rotate-180" />
+    </a>
 
-  <a
-    href="/discover?p={currPage + 1}{query ? `&q=${query}` : ''}"
-    class={["pagination-btn", currPage === maxPage && "pointer-events-none opacity-50"]}
-  >
-    <ChevronRight />
-  </a>
-  <a
-    href="/discover?p={maxPage}{query ? `&q=${query}` : ''}"
-    class={["pagination-btn", currPage === maxPage && "pointer-events-none opacity-50"]}
-  >
-    <ChevronsRight />
-  </a>
-</nav>
+    <!-- this offset makes current page display first by default (while current displays it as second by default) -->
+    <!-- let paginationOffset = $derived(Math.max(0, Math.min(currPage - 1, maxPage - PAGINATION_ITEMS))); -->
+    <!-- {@const page = paginationOffset + i + 1} -->
+    {#each Array(Math.min(maxPage, PAGINATION_ITEMS)) as _, i}
+      {@const page = paginationOffset + i}
+      <a
+        href="/discover?p={page}{query ? `&q=${query}` : ''}"
+        class={["pagination-btn", page === currPage && "bg-fg/20"]}
+      >
+        {page}
+      </a>
+    {/each}
+
+    <a
+      href="/discover?p={currPage + 1}{query ? `&q=${query}` : ''}"
+      class={["pagination-btn", currPage === maxPage && "pointer-events-none opacity-50"]}
+    >
+      <ChevronRight />
+    </a>
+    <a
+      href="/discover?p={maxPage}{query ? `&q=${query}` : ''}"
+      class={["pagination-btn", currPage === maxPage && "pointer-events-none opacity-50"]}
+    >
+      <ChevronsRight />
+    </a>
+  </nav>
+{/if}
