76d787f66fe9de8bc5cb85a2bfcdfb85f8cb8ad0
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-07-14 06:30:26 -0700

    add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Page Not Found ;(</h1>
+      <p className="mt-4 text-lg">
+        The page you are looking for does not exist.
+      </p>
+    </div>
+  );
+}
