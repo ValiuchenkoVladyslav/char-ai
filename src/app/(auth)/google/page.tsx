5b466100d90aff150ae0d1a2e38e283e096499b4
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-07-18 10:12:13 -0700

    fix router import

--- src/app/(auth)/google/page.tsx
+++ src/app/(auth)/google/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useActionState, useLayoutEffect } from "react";
 
 import { createUserOAuth2, setAuth } from "~/modules/auth/client";
