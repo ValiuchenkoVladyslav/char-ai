85e824ff4617745bba59d2e264eb54b99df3393b
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-07-18 09:11:08 -0700

    change alg & add encryption utils

--- src/shared/lib/jwt.ts
+++ src/shared/lib/jwt.ts
@@ -1,40 +1,59 @@
 import "server-only";
 
+import { jwtDecrypt } from "jose/jwt/decrypt";
+import { EncryptJWT } from "jose/jwt/encrypt";
 import { SignJWT } from "jose/jwt/sign";
 import { jwtVerify } from "jose/jwt/verify";
-import { importPKCS8, importSPKI } from "jose/key/import";
 
-export const JWT_ALG = "RS256";
+import { importJWK } from "jose/key/import";
 
-/** fix key after env file */
-function fixKey(key: string) {
-  return key.replaceAll("\\n", "\n");
+const JWT_ALG = "HS512";
+
+const PARSED_SIGN_JWK = JSON.parse(process.env.JWK_SIGN);
+async function importSignJWK() {
+  return importJWK(PARSED_SIGN_JWK, JWT_ALG);
 }
 
-export function encodeKey() {
-  return importPKCS8(fixKey(process.env.JWT_SECRET_ENCODE), JWT_ALG);
-}
-
-export function decodeKey() {
-  return importSPKI(fixKey(process.env.JWT_SECRET_DECODE), JWT_ALG);
-}
-
-export async function signJWT(expiry: string, subject: string) {
-  const key = await encodeKey();
+export async function signJWT(exp: string, sub: string) {
+  const key = await importSignJWK();
 
   return new SignJWT()
     .setProtectedHeader({ alg: JWT_ALG })
-    .setExpirationTime(expiry)
-    .setSubject(subject)
+    .setExpirationTime(exp)
+    .setSubject(sub)
     .sign(key);
 }
 
 export async function verifyJWT(token: string) {
-  try {
-    return await jwtVerify(token, await decodeKey(), {
-      algorithms: [JWT_ALG],
-    });
-  } catch {
-    return undefined;
-  }
+  const key = await importSignJWK();
+
+  return jwtVerify(token, key)
+    .then((res) => res.payload.sub)
+    .catch(() => undefined);
+}
+
+const EJWT_ALG = "A256GCMKW";
+const EJWT_ENC = "A256GCM";
+
+const PARSED_ENC_JWK = JSON.parse(process.env.JWK_ENCRYPT);
+async function importEncryptionJWK() {
+  return importJWK(PARSED_ENC_JWK, EJWT_ALG);
+}
+
+export async function encryptJWT(exp: string, sub: string) {
+  const key = await importEncryptionJWK();
+
+  return new EncryptJWT()
+    .setProtectedHeader({ alg: EJWT_ALG, enc: EJWT_ENC })
+    .setExpirationTime(exp)
+    .setSubject(JSON.stringify(sub))
+    .encrypt(key);
+}
+
+export async function decryptJWT(token: string) {
+  const key = await importEncryptionJWK();
+
+  return jwtDecrypt(token, key)
+    .then((res) => res.payload.sub)
+    .catch(() => undefined);
 }
