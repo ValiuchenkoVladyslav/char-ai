0558e6b74532391679a0d9332affd915b0087f72
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-08-12 01:32:45 +0300

    fix schema path & verify db url

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
+if (!process.env.DB_URL) {
+  throw new Error("DB_URL is missing!");
+}
+
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/db/schema.ts",
+  schema: "./src/lib/db/schema.ts",
   out: "./drizzle",
+
+  dbCredentials: {
+    url: process.env.DB_URL,
+  },
 });
