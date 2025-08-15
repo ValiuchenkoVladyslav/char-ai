68dc335e78f6ac34c50ea0c33270bc6369760e10
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-08-15 14:36:06 -0700

    add redis

--- backend/src/lib/db/index.ts
+++ backend/src/lib/db/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
+import { Redis } from "ioredis";
 import postgres from "postgres";
 
 export const db = drizzle({
   client: postgres(process.env.DB_URL),
 });
+
+export const redis = new Redis(process.env.REDIS_URL);
