a7d8a761cb4dcff3a80db46b1d520c263e99eac8
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-08-14 19:05:51 -0700

    rename argon secret to hmac, abstart out to crypto module

--- backend/scripts/gen-hmac-secret.ts
+++ backend/scripts/gen-hmac-secret.ts
@@ -2,4 +2,4 @@ import { randomBytes } from "node:crypto";
 
 const SECRET_BYTES = 64; // 512 bits
 
-console.log(`ARGON2_SECRET:\n${randomBytes(SECRET_BYTES).toString("base64")}`);
+console.log(`HMAC_SECRET:\n${randomBytes(SECRET_BYTES).toString("base64")}`);
---
+++ backend/src/lib/crypto.ts
@@ -0,0 +1,23 @@
+import { createHmac } from "node:crypto";
+
+export function hmac(password: string) {
+  return createHmac("sha256", process.env.HMAC_SECRET)
+    .update(password)
+    .digest();
+}
+
+export namespace Argon2 {
+  const options: Bun.Password.Argon2Algorithm = {
+    algorithm: "argon2id",
+    memoryCost: 19 * 1024,
+    timeCost: 2,
+  };
+
+  export function hash(str: string) {
+    return Bun.password.hashSync(hmac(str), options);
+  }
+
+  export function verify(str: string, hashed: string) {
+    return Bun.password.verifySync(hmac(str), hashed);
+  }
+}
