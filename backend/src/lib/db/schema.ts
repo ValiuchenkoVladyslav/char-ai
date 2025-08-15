04fd55ea84ac559d7598b3604868dbebe5369f97
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-08-14 19:08:08 -0700

    add session table & general schema improvements

--- backend/src/lib/db/schema.ts
+++ backend/src/lib/db/schema.ts
@@ -7,65 +7,95 @@ import {
   userNameBase,
 } from "@repo/schema";
 import {
+  index,
   integer,
   pgTable,
   primaryKey,
-  serial,
   smallint,
+  timestamp,
+  uniqueIndex,
   varchar,
 } from "drizzle-orm/pg-core";
 import { type SQL, sql } from "drizzle-orm/sql";
 
-export const userTbl = pgTable("User", {
-  id: serial("id").primaryKey(),
+export const userTbl = pgTable(
+  "User",
+  {
+    id: integer("id").generatedAlwaysAsIdentity().primaryKey(),
 
-  tag: varchar("tag", { length: tagBase.maxLen }).notNull().unique(),
-  name: varchar("name", { length: userNameBase.maxLen }).notNull(),
-  pfp: varchar("pfp"),
+    tag: varchar("tag", { length: tagBase.maxLen }).notNull().unique(),
+    name: varchar("name", { length: userNameBase.maxLen }).notNull(),
+    pfpUrl: varchar("pfpUrl"),
 
-  email: varchar("email", { length: emailBase.maxLen }).notNull().unique(),
-  passwordHash: varchar("passwordHash"),
-  googleId: varchar("googleId").unique(),
-  authMethod: smallint("authMethod").notNull(),
-});
+    email: varchar("email", { length: emailBase.maxLen }).notNull().unique(),
+    passwordHash: varchar("passwordHash"),
+    googleId: varchar("googleId").unique(),
+    authMethod: smallint("authMethod").notNull(),
+  },
+  (table) => [
+    uniqueIndex("userTagIdx").on(table.tag), // user lookups
+  ],
+);
 
-export const characterTbl = pgTable("Character", {
-  id: serial("id").primaryKey(),
+export const sessionTbl = pgTable(
+  "Session",
+  {
+    id: integer("id").generatedAlwaysAsIdentity().primaryKey(),
 
-  name: varchar("name", { length: characterNameBase.maxLen }).notNull(),
-  phonetics: varchar("phonetics", { length: 32 })
-    .generatedAlwaysAs((): SQL => sql`metaphone(${characterTbl.name}, 32)`)
-    .notNull(),
+    tokenHash: varchar("tokenHash").notNull(),
+    expiresAt: timestamp("expiresAt").notNull(),
 
-  description: varchar("description", {
-    length: descriptionBase.maxLen,
-  }).notNull(),
+    userId: integer("userId").references(() => userTbl.id, {
+      onDelete: "cascade",
+    }),
+  },
+  (table) => [
+    index("tokenHashIdx").on(table.tokenHash), // session lookups
+  ],
+);
 
-  pfpUrl: varchar("pfpUrl").notNull(),
-  coverImageUrl: varchar("coverImageUrl").notNull(),
+export const characterTbl = pgTable(
+  "Character",
+  {
+    id: integer("id").generatedAlwaysAsIdentity().primaryKey(),
 
-  prompt: varchar("prompt", { length: promptBase.maxLen }).notNull(),
+    name: varchar("name", { length: characterNameBase.maxLen }).notNull(),
+    phonetics: varchar("phonetics", { length: 32 })
+      .generatedAlwaysAs((): SQL => sql`metaphone(${characterTbl.name}, 32)`)
+      .notNull(),
 
-  likesCount: integer("likesCount").notNull().default(0),
+    description: varchar("description", {
+      length: descriptionBase.maxLen,
+    }).notNull(),
 
-  creatorId: integer("creatorId").references(() => userTbl.id, {
-    onDelete: "set null",
-  }),
-});
+    pfpUrl: varchar("pfpUrl").notNull(),
+    coverImageUrl: varchar("coverImageUrl").notNull(),
+
+    prompt: varchar("prompt", { length: promptBase.maxLen }).notNull(),
+
+    creatorId: integer("creatorId").references(() => userTbl.id, {
+      onDelete: "set null",
+    }),
+  },
+  (table) => [
+    index("phoneticsIdx").on(table.phonetics), // search happens by phonetics
+  ],
+);
 
 export const likeTable = pgTable(
   "Like",
   {
-    characterId: integer("characterId")
-      .notNull()
-      .references(() => characterTbl.id, {
-        onDelete: "cascade",
-      }),
-    userId: integer("userId")
-      .notNull()
-      .references(() => userTbl.id, {
-        onDelete: "cascade",
-      }),
+    characterId: integer("characterId").references(() => characterTbl.id, {
+      onDelete: "cascade",
+    }),
+    userId: integer("userId").references(() => userTbl.id, {
+      onDelete: "cascade",
+    }),
   },
-  (table) => [primaryKey({ columns: [table.characterId, table.userId] })],
+  (table) => [
+    primaryKey({ columns: [table.characterId, table.userId] }),
+
+    index("characterIdIdx").on(table.characterId), // to count how many likes a character has
+    index("userIdIdx").on(table.userId), // find out if character is liked by user, get all likes by user
+  ],
 );
