42870515afed642a56ae4252c6ce065b0edc6472
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-08-16 17:43:18 -0700

    add pfp validation

--- backend/src/modules/user/controller.ts
+++ backend/src/modules/user/controller.ts
@@ -1,11 +1,22 @@
 import { zValidator } from "@hono/zod-validator";
 import { confirmEmailDto, signUpDto } from "@repo/schema";
 import { Hono } from "hono";
+import { UserImage } from "./lib/user-image";
 import { handleSignUpForm, signUpEmailPass } from "./services/sign-up";
 
 export const userController = new Hono()
-  .post("/sign-up", zValidator("form", signUpDto), (ctx) => {
-    return handleSignUpForm(ctx, ctx.req.valid("form"));
+  .post("/sign-up", zValidator("form", signUpDto), async (ctx) => {
+    const data = ctx.req.valid("form");
+
+    // validate pfp
+    if (data.pfp) {
+      const pfpValidationRes = await UserImage.validatePfp(data.pfp);
+      if (pfpValidationRes instanceof Error) {
+        return ctx.text(`Invalid pfp! ${pfpValidationRes.message}`, 400);
+      }
+    }
+
+    return handleSignUpForm(ctx, data);
   })
   .post("/sign-up/confirm", zValidator("form", confirmEmailDto), (ctx) => {
     return signUpEmailPass(ctx, ctx.req.valid("form"));
